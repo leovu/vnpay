4438175c856f0cc176ea6b7233f8e47cfc5d241a
nguyenduyan <nguyenduyan.annd@gmail.com>
2020-11-26 11:20:44 +0700

    push

--- android/src/main/kotlin/wao/flutter/application/project/vnpay/VnpayPlugin.kt
+++ android/src/main/kotlin/wao/flutter/application/project/vnpay/VnpayPlugin.kt
@@ -16,6 +16,7 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
+import java.lang.Exception
 
 
 /** VnpayPlugin */
@@ -77,9 +78,6 @@ class VnpayPlugin: FlutterPlugin, MethodCallHandler, ActivityAware , PluginRegis
           }
         }
       }
-    }
-    catch (e: SomeException) {
-      // handler
     }
     return true
   }
